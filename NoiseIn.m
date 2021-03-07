4f3679b2e424fcbef7ecd1a6112b51d68da715bc
Yilgor <49410530+Yilgor@users.noreply.github.com>
2021-03-07 21:33:52 +0100

    Update NoiseIn.m

--- NoiseIn.m
+++ NoiseIn.m
@@ -1,10 +1,10 @@
-function [Wn, SNRin] = NoiseIn(S, F, Fs, Alpha, Bt, N)
+function [Wn] = NoiseIn(S, F, Fs, SNR, Bt, N)
     
     % INPUT
         % S = Input signal
         % F = Symbol frequency.
         % Fs = Sample frequency
-        % Alpha => Relation Eb = No*Alpha with Alpha in dB.
+        % SNR => Relation 2E = No*SNR with SNR given in dB.
         % Bt = Bandwidth
         % N = Number of symbol
     % OUTPUT
@@ -16,24 +16,20 @@ function [Wn, SNRin] = NoiseIn(S, F, Fs, Alpha, Bt, N)
     % (plus précisément, en sortie du modulateur FM).
     
     %%%%%% Calcul de la puissance du signal reçu à l'entré du recepteur Pe.
-    Pe = sum(S.^2)/length(S);
+    %Pe = sum(S.^2)/length(S);
     %Pe = sum(S.^2)/Fs;
-    %Pe = (1/Fs)*trapz(S.^2);
+    Pe = (1/Fs)*trapz(S.^2);
     
     %%%%%% Calcul de la puissance du bruit Pn.
     Eb = Pe/F;
         % Eb = L'énergie d'un bit.
         % 1/F = T = Durée d'un bit.
-    No = Eb/(10^(Alpha/10));
+    No = Eb/(10^(SNR/10));
         % No = Densité spectrale de puissance ([No] = W/Hz).
-        % Alpha [dB] = 10*log10(Eb/No)
-        % <=> Eb/No = 10^(Alpha/10)
-        % <=> No = Eb/(10^(Alpha/10))
+        % SNR [dB] = 10*log10(Eb/No)
+        % <=> Eb/No = 10^(SNR/10)
+        % <=> No = Eb/(10^(SNR/10))
     Pn = (No/2)*Fs;
-
-    %%%%%% Calcul de SNRin
-    SNRin = 10*log10(Pe/No*Bt);
-        % SNRin est donné en dB.
 
     %%%%%% Calcul du signal bruité.
     Wn = sqrt(Pn)*randn([1, N*Fs/F]);
